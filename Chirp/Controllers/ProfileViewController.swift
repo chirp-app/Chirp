c8a3468da5145b9ea528fb68f5e472167d8ada24
evanorourke <49412929+evanorourke@users.noreply.github.com>
2021-05-03 15:21:50 -0600

    Replacing ProfileViewController class definition, adding ProfileView.swift

--- Chirp/Controllers/ProfileViewController.swift
+++ Chirp/Controllers/ProfileViewController.swift
@@ -8,133 +8,149 @@
 import UIKit
 import FirebaseAuth
 
+//class ProfileViewController: UIViewController {
+//
+////    @IBOutlet var tableView: UITableView!
+//    internal var tableView: UITableView = {
+//        let table = UITableView()
+////        table.backgroundColor = UIColor.white
+//        table.translatesAutoresizingMaskIntoConstraints = false
+//        return table
+//      } ()
+//
+//    let data = ["Log Out"]
+//
+//    init() {
+//        super.init(nibName: nil, bundle: nil)
+//
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//      fatalError("init(coder:) has not been implemented.")
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .white
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.tableHeaderView = createTableHeader()
+//    }
+//
+//    func createTableHeader() -> UIView? {
+//        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+//            return nil
+//        }
+//
+//        let safeEmail = DatabaseManager.safeEmail(email: email)
+//        let filename = safeEmail + "_profile_picture.png"
+//        let path = "images/"+filename
+//
+//        let headerView = UIView(frame: CGRect(x:0,
+//                                        y:0,
+//                                        width: self.view.width,
+//                                        height: 300))
+//
+//        headerView.backgroundColor = .link
+//
+//        let imageView = UIImageView(frame: CGRect(x: (view.width - 150)/2,
+//                                        y:75,
+//                                        width: 150,
+//                                        height: 150))
+//
+//        imageView.contentMode = .scaleAspectFill
+//        imageView.backgroundColor = .white
+//        imageView.layer.borderWidth = 3
+//        imageView.layer.masksToBounds = true
+//        imageView.layer.cornerRadius = imageView.width/2
+//        headerView.addSubview(imageView)
+//
+//        StorageManager.shared.downloadURL(for: path, completion: { [weak self] result in
+//            switch result {
+//            case .success(let url):
+//                self?.downloadImage(imageView: imageView, url: url)
+//            case .failure(let error):
+//                print("Failed to get download url: \(error)")
+//            }
+//        })
+//        return headerView
+//    }
+//
+//    func downloadImage(imageView: UIImageView, url: URL){
+//        URLSession.shared.dataTask(with: url, completionHandler: {data, _, error in
+//            guard let data = data, error == nil else{
+//                return
+//            }
+//
+//            DispatchQueue.main.async{
+//                let image = UIImage(data:data)
+//                imageView.image = image
+//            }
+//        }).resume()
+//    }
+//}
+
+
+
+
+//extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return data.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = data[indexPath.row]
+//        cell.textLabel?.textAlignment = .center
+//        cell.textLabel?.textColor = .red
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+//        tableView.deselectRow(at: indexPath, animated: true)
+//
+//        let actionSheet = UIAlertController(title:nil, message:nil, preferredStyle: .actionSheet)
+//
+//        actionSheet.addAction(UIAlertAction(
+//            title: "Log Out",
+//            style: .destructive,
+//            handler: { [weak self] _ in
+//
+//                guard let strongSelf = self else{
+//                    return
+//                }
+//                            do {
+//                                try FirebaseAuth.Auth.auth().signOut()
+//
+//                                let vc = LoginViewController()
+//                                let nav = UINavigationController(rootViewController: vc)
+//                                nav.modalPresentationStyle = .fullScreen
+//                                strongSelf.present(nav, animated: true)
+//                                }
+//                            catch{
+//                                print("Failed to log out")
+//                            }
+//                    }
+//        ))
+//        actionSheet.addAction(UIAlertAction(title: "Cancel",
+//                                            style: .cancel,
+//                                            handler: nil))
+//
+//        present(actionSheet, animated: true)
+//    }
+//}
+//
+
+
 class ProfileViewController: UIViewController {
     
-//    @IBOutlet var tableView: UITableView!
-    internal var tableView: UITableView = {
-        let table = UITableView()
-//        table.backgroundColor = UIColor.white
-        table.translatesAutoresizingMaskIntoConstraints = false
-        return table
-      } ()
-    
-    let data = ["Log Out"]
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
+    var profileView: ProfileView {
+        return view as! ProfileView
     }
     
-    required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented.")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.tableHeaderView = createTableHeader()
-    }
-    
-    func createTableHeader() -> UIView? {
-        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
-            return nil
-        }
-        
-        let safeEmail = DatabaseManager.safeEmail(email: email)
-        let filename = safeEmail + "_profile_picture.png"
-        let path = "images/"+filename
-        
-        let headerView = UIView(frame: CGRect(x:0,
-                                        y:0,
-                                        width: self.view.width,
-                                        height: 300))
-        
-        headerView.backgroundColor = .link
-        
-        let imageView = UIImageView(frame: CGRect(x: (view.width - 150)/2,
-                                        y:75,
-                                        width: 150,
-                                        height: 150))
-        
-        imageView.contentMode = .scaleAspectFill
-        imageView.backgroundColor = .white
-        imageView.layer.borderWidth = 3
-        imageView.layer.masksToBounds = true
-        imageView.layer.cornerRadius = imageView.width/2
-        headerView.addSubview(imageView)
-        
-        StorageManager.shared.downloadURL(for: path, completion: { [weak self] result in
-            switch result {
-            case .success(let url):
-                self?.downloadImage(imageView: imageView, url: url)
-            case .failure(let error):
-                print("Failed to get download url: \(error)")
-            }
-        })
-        return headerView
-    }
-    
-    func downloadImage(imageView: UIImageView, url: URL){
-        URLSession.shared.dataTask(with: url, completionHandler: {data, _, error in
-            guard let data = data, error == nil else{
-                return
-            }
-            
-            DispatchQueue.main.async{
-                let image = UIImage(data:data)
-                imageView.image = image
-            }
-        }).resume()
+    override func loadView() {
+        view = ProfileView(frame: UIScreen.main.bounds)
     }
 }
-
-extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
-        cell.textLabel?.textAlignment = .center
-        cell.textLabel?.textColor = .red
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let actionSheet = UIAlertController(title:nil, message:nil, preferredStyle: .actionSheet)
-        
-        actionSheet.addAction(UIAlertAction(
-            title: "Log Out",
-            style: .destructive,
-            handler: { [weak self] _ in
-                
-                guard let strongSelf = self else{
-                    return
-                }
-                            do {
-                                try FirebaseAuth.Auth.auth().signOut()
-                                
-                                let vc = LoginViewController()
-                                let nav = UINavigationController(rootViewController: vc)
-                                nav.modalPresentationStyle = .fullScreen
-                                strongSelf.present(nav, animated: true)
-                                }
-                            catch{
-                                print("Failed to log out")
-                            }
-                    }
-        ))
-        actionSheet.addAction(UIAlertAction(title: "Cancel",
-                                            style: .cancel,
-                                            handler: nil))
-        
-        present(actionSheet, animated: true)
-    }
-}
-
---
+++ Chirp/Views/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Chirp
+//
+//  Created by Evan ORourke on 5/3/21.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented.")
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+}
